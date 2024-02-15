5b886dff6a5c81e165ec1d3e58eb8b3bfccf03d4
Daniel Musau <136645134+dmusau23@users.noreply.github.com>
2024-02-14 23:26:52 -0500

    Add files via upload
    first commit

---
+++ MultiPoints.java
@@ -0,0 +1,13 @@
+package PointDemo;
+
+public class MultiPoints {
+    private  double x;
+    private double y;
+
+    public MultiPoints(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+}
---
+++ PointTester.java
@@ -0,0 +1,9 @@
+package PointDemo;
+
+public class PointTester {
+    public static void main(String[] args) {
+        //create an object of either Point or NamedPoint
+        NamedPoint myPoint = new NamedPoint(2.0,5.0,"a");
+        System.out.println(myPoint);
+    }
+}
